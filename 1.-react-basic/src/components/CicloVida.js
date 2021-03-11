5856082c9e466683f6402c2b5a6baeb633b6d6f5
jonmolinna <jmolina2624@gmail.com>
2021-03-11 15:57:23 -0500

    Ciclo de Vida class

--- 1.-react-basic/src/components/CicloVida.js
+++ 1.-react-basic/src/components/CicloVida.js
@@ -18,7 +18,7 @@ class Reloj extends Component {
 export default class CicloVida extends Component {
     constructor(props){
         super(props);
-        console.log(0, "El componente se inicializa, aun NO esta en el DOM");
+        //console.log(0, "El componente se inicializa, aun NO esta en el DOM");
 
         this.state = {
             hora: new Date().toLocaleTimeString(),
@@ -30,14 +30,14 @@ export default class CicloVida extends Component {
 
     // Componente de Montaje, aqui se ejecutan las peticiones
     componentDidMount(){
-        console.log(1, 'El componente ya se encuentra en el DOM');
+        //console.log(1, 'El componente ya se encuentra en el DOM');
     }
 
     // Componente de Actualizacion
     componentDidUpdate(prevProps, prevState){
-        console.log(2, "El estado o las props del componente han cambiado");
-        console.log(prevProps);
-        console.log(prevState);
+        //console.log(2, "El estado o las props del componente han cambiado");
+        //console.log(prevProps);
+        //console.log(prevState);
     }
 
     tictac = () => {
@@ -63,7 +63,7 @@ export default class CicloVida extends Component {
     }
 
     render(){
-        console.log(4, "El componente se dibuja (o redibuja por algun cambio) en el DOM")
+        //console.log(4, "El componente se dibuja (o redibuja por algun cambio) en el DOM")
 
         return (
             <>
